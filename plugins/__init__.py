dc84afcacd3542079185fd43c85da07d6a3aff9f
𝐀ʀᴄʜᴏɴ [🇮🇳] <teamoctavex@gmail.com>
2026-09-01 12:23:59 +0530

    Add files via upload

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,1 +1,15 @@
+ALL_MODULES = [
+    "/bot/start",
+    "/bot/help",
+    "/bot/callbacks",
+    "/bot/logs",
+    "/bot/settings",
+    "/bot/tracker",
+    "/bot/owner",
 
+    "/play/play",
+    "/play/queue",
+
+    "/admins/controls",
+    "/admins/loop",
+]
